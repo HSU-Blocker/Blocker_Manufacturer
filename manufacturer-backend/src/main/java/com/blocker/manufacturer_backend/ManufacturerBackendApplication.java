5cc8bb2d12c3333cf6de28e096c8aa36a731af20
3DUCK <kermit7520k@gmail.com>
2025-03-10 01:30:21 +0900

    FEAT: BackEnd start

---
+++ manufacturer-backend/src/main/java/com/blocker/manufacturer_backend/ManufacturerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blocker.manufacturer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturerBackendApplication.class, args);
+	}
+
+}
---
+++ manufacturer-backend/src/test/java/com/blocker/manufacturer_backend/ManufacturerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blocker.manufacturer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManufacturerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
